9cddda0e5d32243dfaa974213ee33380061f2663
Censure <hugodubois1462@gmail.com>
2024-11-29 18:37:13 +0100

    loader.lua

---
+++ loader.lua
@@ -0,0 +1,4 @@
+-- Made by Censure
+-- This system is not open source and is obfuscate
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Astral-UI-Off/AstralSystem/refs/heads/main/AstralLoad"))()
